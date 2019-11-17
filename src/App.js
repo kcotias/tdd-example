c468a8f149e6037bcf52b3383bcd9b69ff543d59
kcotias <kaianexperia@gmail.com>
2019-11-16 22:47:23 -0300

    finish example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
   render() {
     return (
       <div data-test="component-app">
-        <h1>Ola mundo</h1>
+        <h1 data-test="counter-display">{this.state.counter}</h1>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          data-test="increment-button"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            this.state.counter === 0
+              ? alert("Counter is zero")
+              : this.setState({ counter: this.state.counter - 1 });
+          }}
+          data-test="decrement-button"
+        >
+          Decrement
+        </button>
       </div>
     );
   }
